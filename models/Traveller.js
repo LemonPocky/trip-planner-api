7521a333bbe805a8146edad802cd665aa9bfc64e
LemonPocky <someone-70@live.com>
2021-07-22 10:46:16 -0700

    Added traveller columns

--- models/Traveller.js
+++ models/Traveller.js
@@ -8,6 +8,24 @@ class Traveller extends Model {}
 Traveller.init(
   {
     // TODO: configure columns
+        id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
   },
   {
     sequelize,

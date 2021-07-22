1d4b20f1e2c391f08875c4384b77cbd0b09de1a2
LemonPocky <someone-70@live.com>
2021-07-22 10:46:38 -0700

    Added many to many relationship

--- models/index.js
+++ models/index.js
@@ -3,5 +3,7 @@ const Location = require("./Location");
 const Trip = require("./Trip");
 
 // TODO: Setup many-to-many relationship
+Traveller.belongsToMany(Location, {through: Trip});
+Location.belongsToMany(Traveller, {through: Trip});
 
 module.exports = { Traveller, Location, Trip };

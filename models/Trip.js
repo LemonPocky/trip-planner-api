cf0d29aecee9c072f0d25b22681d081dce62ae43
Vivi Cowan <vivicare@gmail.com>
2021-07-22 10:51:39 -0700

    created trip model

--- models/Trip.js
+++ models/Trip.js
@@ -8,6 +8,34 @@ class Trip extends Model {}
 Trip.init(
   {
     // TODO: configure columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    trip_budget: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    traveller_amount: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    traveller_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'traveller',
+        key: 'id',
+      },
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'location',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,

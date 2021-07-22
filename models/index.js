fb4333fdddd084f7e84a821ec2f7dab4055a865e
LemonPocky <someone-70@live.com>
2021-07-22 10:53:09 -0700

    Added cascade delete

--- models/index.js
+++ models/index.js
@@ -3,7 +3,13 @@ const Location = require("./Location");
 const Trip = require("./Trip");
 
 // TODO: Setup many-to-many relationship
-Traveller.belongsToMany(Location, {through: Trip});
-Location.belongsToMany(Traveller, {through: Trip});
+Traveller.belongsToMany(Location, {
+  through: Trip,
+  onDelete: 'CASCADE',
+});
+Location.belongsToMany(Traveller, {
+  through: Trip,
+  onDelete: 'CASCADE',
+});
 
 module.exports = { Traveller, Location, Trip };
